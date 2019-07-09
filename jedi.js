c4210eaf5b1256f8752da6dc38d04ab5c1e57ea7
cmpowers1981 <c.m.powers@outlook.com>
2019-07-09 05:41:51 -0400

    first Commit for Jedi

--- jedi.js
+++ jedi.js
@@ -1,1 +1,10 @@
+'use strict';
 function jediName(firstName, lastName) {
+    let first = firstName.slice(0, 2);
+    let last = lastName.slice(0,3);
+
+    return last + first;
+}
+
+const output = jediName('Chris', 'Powers')
+console.log(output);
