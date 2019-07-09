28af725234c7b749ad8fdf802b5a454c8c034f1c
cmpowers1981 <c.m.powers@outlook.com>
2019-07-09 08:28:51 -0400

    Typo for decoded was detected so I am commiting this one more time

--- crackingTheCode.js
+++ crackingTheCode.js
@@ -2,46 +2,46 @@
 function crackingTheCode(code) {
     
     const words = code.split(' ');
-    let decode = '';
+    let decoded = '';
         words.forEach(function(words) {
             
             if (word[0] == 'a') {
-                decode += word[1];
+                decoded += word[1];
             }
             
             else if (word[0] == 'b') {
-                decode += word[2];
+                decoded += word[2];
             }
             
             else if (word[0] == 'c') {
-                decode += word[3];
+                decoded += word[3];
             }
             
             else if (word[0] == 'd') {
-                decode += word[4];
+                decoded += word[4];
             }
             
             else if (word[0] == 'e') {
-                decode += word[5];
+                decoded += word[5];
             }
 
             else if (word[0] == 'f') {
-                decode += word[6];
+                decoded += word[6];
             }
 
             else if (word[0] == 'g') {
-                decode += word[7];
+                decoded += word[7];
             }
 
             else if (word[0] == 'h') {
-                decode += word[8];
+                decoded += word[8];
             }
 
             else {
-                decode += ' ';
+                decoded += ' ';
             }
         }
-        return decode;
+        return decoded;
 }
 
    console.log(crackingTheCode('craft, block, argon, meter, bells, brown, croon, droop'));
