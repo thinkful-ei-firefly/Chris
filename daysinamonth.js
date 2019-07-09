f3776e68136cc91603970a6065206068fb979810
cmpowers1981 <c.m.powers@outlook.com>
2019-07-09 07:25:42 -0400

    final commit - not I have about 88 errors from lint. all of which are indentation, I did this to organize my work, I am not sure if it is correct proceedure, but this was a long process and I wanted to get this correct.

--- daysinamonth.js
+++ daysinamonth.js
@@ -1,62 +1,62 @@
-'use strict'
+'use strict';
 
-function daysInAMonth(month, leapyear = false) {
+function howManyDaysInAMonth(month, leapYear = false) {
    
     let result = '';
     switch (month) {
         
-        case 'January';
-                result = `${month} has 31 days`;
+        case 'January':
+                result = '${month} has 31 days';
                 break;
         
-        case 'February';
-            if (leapyear) {
-                result = `${month} has 29 days`;
+        case 'February':
+            if (leapYear) {
+                result = '${month} has 29 days';
             } else {
-                result = `${monh} has 28 days`;
+                result = '${month} has 28 days';
             }
             break;
             default:
                 throw new Error('Must provide the correct month.');
 
-        case 'March';
-                result = `$(month) has 31 days`;
+        case 'March':
+                result = '$(month) has 31 days';
                 break;
 
-        case 'April';
-                result = `$(month) has 30 days`;
+        case 'April':
+                result = '$(month) has 30 days';
                 break;
 
-        case 'May';
-                result = `$(month) has 31 days`;
+        case 'May':
+                result = '$(month) has 31 days';
                 break;
 
-        case 'June';
-                result = `$(month) has 30 days`;
+        case 'June':
+                result = '$(month) has 30 days';
                 break;
 
-        case 'July';
-                result = `$(month) has 31 days`;
+        case 'July':
+                result = '$(month) has 31 days';
                 break;
 
-        case 'August';
-                result = `$(month) has 31 days`;
+        case 'August':
+                result = '$(month) has 31 days';
                 break;
 
-        case 'September';
-                result = `$(month) has 30 days`;
+        case 'September':
+                result = '$(month) has 30 days';
                 break;
 
-        case 'October';
-                result = `$(month) has 31 days`;
+        case 'October':
+                result = '$(month) has 31 days';
                 break;
 
-        case 'November';
-                result = `$(month) has 30 days`;
+        case 'November':
+                result = '$(month) has 30 days';
                 break;
 
-        case 'December';
-                result =  `$(month) has 31 days`;
+        case 'December':
+                result =  '$(month) has 31 days';
                 break;
 
     }
@@ -64,19 +64,20 @@ function daysInAMonth(month, leapyear = false) {
 }
 
 try {
-    console.log(daysInAMonth('January'));
-    console.log(daysInAMonth('February', false));
-    console.log(daysInAMonth('February', true));
-    console.log(daysInAMonth('March'));
-    console.log(daysInAMonth('April'));
-    console.log(daysInAMonth('May'));
-    console.log(daysInAMonth('June'));
-    console.log(daysInAMonth('July'));
-    console.log(daysInAMonth('August'));
-    console.log(daysInAMonth('September'));
-    console.log(daysInAMonth('October'));
-    console.log(daysInAMonth('November'));
-    console.log(daysInAMonth('December'));
-    console.log(daysInAMonth('Sept'));
-} catch(e);
+    console.log(howManyDaysInAMonth('January'));
+    console.log(howManyDaysInAMonth('February', false));
+    console.log(howManyDaysInAMonth('February', true));
+    console.log(howManyDaysInAMonth('March'));
+    console.log(howManyDaysInAMonth('April'));
+    console.log(howManyDaysInAMonth('May'));
+    console.log(howManyDaysInAMonth('June'));
+    console.log(howManyDaysInAMonth('July'));
+    console.log(howManyDaysInAMonth('August'));
+    console.log(howManyDaysInAMonth('September'));
+    console.log(howManyDaysInAMonth('October'));
+    console.log(howManyDaysInAMonth('November'));
+    console.log(howManyDaysInAMonth('December'));
+    console.log(howManyDaysInAMonth('Sept'));
+}catch(e) {
     console.log(e.message);
+}
